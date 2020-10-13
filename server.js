985603a7e8c6113642d6d220ecefd4b15d0ec907
unknown <nithinkottary09@gmail.com>
2020-10-13 23:31:05 +0530

    comment added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV !== 'production'){
    require('dotenv').config();
 }
-
+// packages
 const express = require('express');
 const app = express();
 const router = require('./routes/index');
